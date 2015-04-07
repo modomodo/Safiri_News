00ea9dd732defd99b32e67d505c153892042372a
Michael Muita <mugz.mike@gmail.com>
2015-04-07 12:52:08 +0800

    Initial commit

---
+++ app/src/main/java/com/example/michael/the_one/HistoryTab.java
@@ -0,0 +1,19 @@
+package com.example.michael.the_one;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Michael on 01/04/2015.
+ */
+public class HistoryTab extends Fragment{
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v =inflater.inflate(R.layout.news_tab,container,false);
+        return v;
+    }
+}
