8d913372cd73b2ef179dd9c38c76748c904acc55
Michael Muita <mugz.mike@gmail.com>
2015-04-07 13:48:12 +0800

    Step 1: Rotten Tomato API integration

--- app/src/main/java/com/example/michael/the_one/MainActivity.java
+++ app/src/main/java/com/example/michael/the_one/MainActivity.java
@@ -68,7 +68,7 @@ public class MainActivity extends ActionBarActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        // as a parent activity in AndroidManifest.xml is specified.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
--- app/src/main/java/com/example/michael/the_one/NewsTab.java
+++ app/src/main/java/com/example/michael/the_one/NewsTab.java
@@ -7,13 +7,37 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+
 /**
  * Created by Michael on 01/04/2015.
  */
 public class NewsTab extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v =inflater.inflate(R.layout.history_tab,container,false);
-        return v;
+        View layout =inflater.inflate(R.layout.history_tab,container,false);
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+
+        StringRequest request = new StringRequest(Request.Method.GET, "", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+            }
+        }, new Response.ErrorListener() {
+
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        return layout; //Return the layout for the fragment
     }
 }
